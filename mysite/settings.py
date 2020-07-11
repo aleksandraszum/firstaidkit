47ae68b92f9728a4ff89e2098cbe3519c9f1db52
Aleksandra Szum <szum.aleksandra@gmail.com>
2020-07-11 21:17:37 +0200

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'change!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [['127.0.0.1', 'apteczka.pythonanywhere.com']]
+ALLOWED_HOSTS = ['127.0.0.1', 'apteczka.pythonanywhere.com']
 
 # Application definition
 
